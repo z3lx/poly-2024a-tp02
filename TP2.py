b07840b72f7239bdedc6db689fd16b058802cb61
z3lx <57017122+z3lx@users.noreply.github.com>
2024-10-04 13:50:03 -0400

    Implement tp5

--- TP2.py
+++ TP2.py
@@ -8,7 +8,8 @@ Noms et matricules : Nom1 (Matricule1), Nom2 (Matricule2)
 
 import csv
 import os
-from typing import Dict
+from datetime import datetime
+from typing import Dict, Union, List
 
 
 def add_collection(
@@ -51,11 +52,17 @@ def add_collection(
                 print(status_message)
 
 
+def elapsed_days(date: str) -> int:
+    given_date = datetime.strptime(date, "%Y-%m-%d")
+    current_date = datetime.now()
+    return (current_date - given_date).days
+
+
 ##########################################################################################################
 # PARTIE 1 : Création du système de gestion et ajout de la collection actuelle
 ##########################################################################################################
 
-library: Dict[str, Dict[str, str]] = {}
+library: Dict[str, Union[Dict[str, Union[str, int]], List[str]]] = {}
 add_collection(library, "collection_bibliotheque.csv")
 print(f' \n Bibliotheque initiale : {library} \n')
 
@@ -88,16 +95,13 @@ with open("emprunts.csv", "r", newline="", encoding="utf-8") as f:
     for row in reader:
         if reader.line_num == 1:
             continue
-
         cote, date = row
         borrows[cote] = date
 
 for cote, book in library.items():
     title, author, year = book.values()
-    borrow_status = "emprunté" if cote in borrows else "disponible"
-    borrow_date = borrows.get(cote, "N/A")
-    library[cote]["emprunts"] = borrow_status
-    library[cote]["date_emprunt"] = borrow_date
+    library[cote]["emprunts"] = "emprunté" if cote in borrows else "disponible"
+    library[cote]["date_emprunt"] = borrows.get(cote, "N/A")
 
 print(f' \n Bibliotheque avec ajout des emprunts : {library} \n')
 
@@ -105,4 +109,18 @@ print(f' \n Bibliotheque avec ajout des emprunts : {library} \n')
 # PARTIE 5 : Livres en retard
 ##########################################################################################################
 
-# TODO : Écrire votre code ici
+lost_books = []
+for cote, book in library.items():
+    title, author, year, borrow_status, borrow_date = book.values()
+    elapsed_late = 0 if borrow_status != "emprunté" else max(elapsed_days(borrow_date) - 30, 0)
+    if elapsed_late > 365:
+        fee = 0
+        lost_books.append(cote)
+    else:
+        fee = min(elapsed_late * 2, 100)
+    library[cote]["frais_retard"] = fee
+    if fee > 0:
+        print(f"Livre '{cote}' en retard: frais de {fee}$")
+library["livres_perdus"] = lost_books
+
+print(f' \n Bibliotheque avec ajout des retards et frais : {library} \n')
