a372efba15952c61841b57ecd0d6a57429be3228
z3lx <57017122+z3lx@users.noreply.github.com>
2024-10-04 09:53:33 -0400

    Implement pt2

--- TP2.py
+++ TP2.py
@@ -7,35 +7,63 @@ Noms et matricules : Nom1 (Matricule1), Nom2 (Matricule2)
 """
 
 import csv
+import os
+from typing import Dict
+
+
+def add_collection(
+    library: Dict[str, Dict[str, str]],
+    collection_path: str,
+    skip_first_line: bool = True,
+    log: bool = False
+) -> None:
+    if not os.path.exists(collection_path):
+        print(f"Le fichier '{collection_path}' n'existe pas.")
+        return
+
+    with open(collection_path, "r", newline="", encoding="utf-8") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if skip_first_line and reader.line_num == 1:
+                continue
+
+            title, author, year, cote = row
+
+            if cote in library:
+                status_message = (
+                    f"Le livre {cote} ---- "
+                    f"{title} par {author} ---- "
+                    f"est déjà présent dans la bibliothèque"
+                )
+            else:
+                library[cote] = {
+                    "titre": title,
+                    "auteur": author,
+                    "date_publication": year
+                }
+                status_message = (
+                    f"Le livre {cote} ---- "
+                    f"{title} par {author} ---- "
+                    f"a été ajouté avec succès"
+                )
+
+            if log:
+                print(status_message)
+
 
 ##########################################################################################################
 # PARTIE 1 : Création du système de gestion et ajout de la collection actuelle
 ##########################################################################################################
 
 bibliotheque = {}
-
-with open("collection_bibliotheque.csv", "r", newline="", encoding="utf-8") as f:
-    reader = csv.reader(f)
-    for row in reader:
-        title, author, year, cote = row
-        bibliotheque[cote] = {
-            "titre": title,
-            "auteur": author,
-            "date_publication": year
-        }
-
+add_collection(bibliotheque, "collection_bibliotheque.csv")
 print(f' \n Bibliotheque initiale : {bibliotheque} \n')
 
 ##########################################################################################################
 # PARTIE 2 : Ajout d'une nouvelle collection à la bibliothèque
 ##########################################################################################################
 
-# TODO : Écrire votre code ici
-
-
-
-
-
+add_collection(bibliotheque, "nouvelle_collection.csv", log=True)
 
 ##########################################################################################################
 # PARTIE 3 : Modification de la cote de rangement d'une sélection de livres
